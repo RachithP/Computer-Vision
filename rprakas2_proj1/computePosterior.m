9b1e8908d98fe88fb0a3dac273645cc56c5b290d
Rachith <rachithprakash@gmail.com>
2018-12-20 19:46:22 -0500

    Panaroma project

--- rprakas2_proj1/computePosterior.m
+++
@@ -1,9 +0,0 @@
-function [PCx] = computePosterior(temp,u,sig,invsig)
-
-Op = 0.4; % Probability of Orange color - Random initialization
-
-N = 3; % This is a constant as we always have RGB component.
-den = 1/sqrt(((2*pi)^N)*det(sig));
-% invsig = inv(sig);
-
-PCx = ((1/den) .* Op .*  exp(-0.5*(((temp-u)')*(invsig)*(temp-u))));
