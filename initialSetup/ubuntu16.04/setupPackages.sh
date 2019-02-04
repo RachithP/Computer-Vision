3592a0cefa24c7756b2d725c43245988e845d469
RachithP <rachithprakash@gmail.com>
2019-02-03 23:19:01 -0500

    Initial packages...git,sublime,nvidia driver

---
+++ initialSetup/ubuntu16.04/setupPackages.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "Installing git..."
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get -y install git
+
+restart
+
+echo "Cloning Computer-Vision github repo..."
+mkdir -p ~/git
+cd ~/git
+git clone https://github.com/RachithP/Computer-Vision.git
+
+cd ~
+echo "Installing sublime-text 3..."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt-get -y install sublime-text
+
+echo "Installing nvidia driver 410 (Latest as of Feb-03-2019)..."
+sudo apt-get purge nvidia*
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt-get -y install nvidia-410
+
+
+
+
+
+
+
+
+
+
