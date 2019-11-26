d361bee19699274f891f106a559ed8a6c6996971
RACHITH <42787281+RachithP@users.noreply.github.com>
2019-11-26 17:42:19 -0500

    Update script_2.sh

--- initialSetup/gcloud-tensorflow/Set-Up-Tensorflow-GPU/script_2.sh
+++ initialSetup/gcloud-tensorflow/Set-Up-Tensorflow-GPU/script_2.sh
@@ -4,4 +4,3 @@ sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
 sudo chmod a+r /usr/local/cuda/include/cudnn.h /usr/local/cuda/lib64/libcudnn*
 
 conda create -n tf python=3.4 pip
-source activate tf
